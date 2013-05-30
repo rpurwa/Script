e61b948fc5fd1fab54ed6ca8774d432ec201cd02
rpurwa <rpurwakala@gmail.com>
2013-05-30 11:17:11 +0800

    Update show.js

--- show.js
+++ show.js
@@ -1,29 +1,3 @@
-$(document).ready(function() {         
-var hash = window.location.hash.substr(1);
-var href = $('#nav li a').each(function(){
-        var href = $(this).attr('href');
-    if(hash==href.substr(0,href.length-4)){
+document.write('<s'+'cript language="JavaScript" src="'+url+'/show.php'"></s'+'cript>'); 
 
-        var toLoad = hash+'.html #content_low'+divId;
-        $('#content'+divId).load(toLoad);
-        alert(divId);
-    }                                           
-});
-
-$('#nav li a').click(function(){
-    divId = $(this).parents('li').attr('id');   
-    var toLoad = $(this).attr('href')+'#content_low'+divId;
-    alert(divId);
-    $('#content'+divId).slideUp('normal',loadContent).delay(200);
-    window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-$(this).attr('href').length);
-    function loadContent() {
-        $('#content'+divId).load(toLoad,showNewContent)
-    }
-    function showNewContent() {
-        $('#content'+divId).delay(700).slideDown($('#content'+divId).height());
-
-    }
-    return false;
-
-});
-});
+//document.write(url+'/show.php?z='+zone+'&w='+w+'&pl='+pl+'&ad_type='+ad_type+'&shape='+shape+'&c_border='+c_border+'&c_background='+c_background+'&c_text1='+c_text1+'&c_text2='+c_text2+'&c_text3='+c_text3+'&c_text4='+c_text4+'&c_text5='+c_text5+'&c_text6='+c_text6+'&c_text7='+c_text7+'&c_text8='+c_text8+'&c_text9='+c_text9+'&c_text10='+c_text10); 
