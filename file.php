8ee7aabc02c27c61e2956a44f459c794fa8d67cf
rpurwa <rpurwakala@gmail.com>
2013-05-30 09:11:23 +0800

    Create file.php

---
+++ file.php
@@ -0,0 +1,6 @@
+<ul id="nav" class="marketlist"><li id="1" class="market twobox">
+<a href="external_file.php"><img src="img/projects/1.jpg" class="items" alt="" /></a>
+</li><li id="2" class="market"> 
+<a href="external_file.php"><img src="img/projects/1.jpg" class="items" alt="" /></a>
+</li>
+</ul>
