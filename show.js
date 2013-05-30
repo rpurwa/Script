71e882bb22544fcbc1238e11a74d2b44ec708837
rpurwa <rpurwakala@gmail.com>
2013-05-30 11:19:20 +0800

    Update show.js

--- show.js
+++ show.js
@@ -1,3 +1,1 @@
 document.write('<s'+'cript language="JavaScript" src="'+url+'/show.php'"></s'+'cript>'); 
-
-//document.write(url+'/show.php?z='+zone+'&w='+w+'&pl='+pl+'&ad_type='+ad_type+'&shape='+shape+'&c_border='+c_border+'&c_background='+c_background+'&c_text1='+c_text1+'&c_text2='+c_text2+'&c_text3='+c_text3+'&c_text4='+c_text4+'&c_text5='+c_text5+'&c_text6='+c_text6+'&c_text7='+c_text7+'&c_text8='+c_text8+'&c_text9='+c_text9+'&c_text10='+c_text10); 
